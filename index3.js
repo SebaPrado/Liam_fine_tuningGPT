db4d9e6ce8d432f8398bd376259fd43e85cec9dd
SebaPrado <sebastian.pradomelesi@gmail.com>
2025-01-11 17:43:54 +0100

    3

--- index3.js
+++ index3.js
@@ -3,67 +3,115 @@ dotenv.config();
 const openai = new OpenAI();
 import OpenAI from "openai";
 
-
 //================================================================================//
 // =======================       Create an assistant       =======================//
 //================================================================================//
 
-let sebasAssistant;
+// let sebasAssistant;
 
-async function createAssitant() {
-const sebasAssistant = await openai.beta.assistants.create({
-  name: "Math Tutor",
-  instructions: "You are a personal math tutor. Write and run code to answer math questions.",
-  tools: [{ type: "code_interpreter" }],
-  model: "gpt-4o"
-});
-console.log("sebasAssistant es : ",sebasAssistant);
+// async function createAssitant() {
+//   const sebasAssistant = await openai.beta.assistants.create({
+//     name: "Math Tutor",
+//     instructions:
+//       "You are a personal math tutor. Write and run code to answer math questions.",
+//     tools: [{ type: "code_interpreter" }],
+//     model: "gpt-4o",
+//   });
+//   console.log("sebasAssistant es : ", sebasAssistant);
+// }
+
+// createAssitant();
+
+// //================================================================================//
+// // ======================         Create a thread        =========================//
+// //================================================================================//
+
+// const thread = await openai.beta.threads.create();
+
+// //================================================================================//
+// // =================       Add a message to the thread        ====================//
+// //================================================================================//
+
+// const message = await openai.beta.threads.messages.create(thread.id, {
+//   role: "user",
+//   content: "I need to solve the equation `3x + 11 = 14`. Can you help me?",
+// });
+
+// //================================================================================//
+// // ========================         Create a run          ========================//
+// //================================================================================//
+
+// let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
+//   sebasAssistant_id: sebasAssistant.id,
+//   instructions:
+//     "Please address the user as Jane Doe. The user has a premium account.",
+// });
+
+// //=========================================================================================//
+// // Once the Run completes, you can list the Messages added to the Thread by the Assistant  //
+// //=========================================================================================//
+
+// if (run.status === "completed") {
+//   const messages = await openai.beta.threads.messages.list(run.thread_id);
+//   for (const message of messages.data.reverse()) {
+//     console.log(`${message.role} > ${message.content[0].text.value}`);
+//   }
+// } else {
+//   console.log(run.status);
+// }
+
+let sebasAssistant; // Definir la variable fuera de la función
+
+async function main() {
+  //================================================================================//
+  // =======================       Crear un asistente       =======================//
+  //================================================================================//
+
+  sebasAssistant = await openai.beta.assistants.create({
+    name: "Math Tutor",
+    instructions:
+      "You are a personal math tutor. Write and run code to answer math questions.",
+    tools: [{ type: "code_interpreter" }],
+    model: "gpt-4o",
+  });
+  console.log("sebasAssistant es : ", sebasAssistant);
+
+  //================================================================================//
+  // ======================         Crear un hilo        =========================//
+  //================================================================================//
+
+  const thread = await openai.beta.threads.create();
+
+  //================================================================================//
+  // =================       Agregar un mensaje al hilo        ====================//
+  //================================================================================//
+
+  const message = await openai.beta.threads.messages.create(thread.id, {
+    role: "user",
+    content: "I need to solve the equation `3x + 11 = 14`. Can you help me?",
+  });
+
+  //===============================================================================//
+  // ====================         Crear una ejecución          ====================//
+  //===============================================================================//
+
+  let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
+    assistant_id: sebasAssistant.id,
+    instructions: "Please address the user as Elmatias..",
+  });
+
+  //=====================================================================================================//
+  // Una vez que la ejecución se completa, puedes listar los mensajes añadidos al hilo por el asistente  //
+  //=====================================================================================================//
+
+  if (run.status === "completed") {
+    const messages = await openai.beta.threads.messages.list(run.thread_id);
+    for (const message of messages.data.reverse()) {
+      console.log(`${message.role} > ${message.content[0].text.value}`);
+    }
+  } else {
+    console.log(run.status);
+  }
 }
 
-createAssitant();
-
-//================================================================================//
-// ======================         Create a thread        =========================//
-//================================================================================//
-
-const thread = await openai.beta.threads.create();
-
-//================================================================================//
-// =================       Add a message to the thread        ====================//
-//================================================================================//
-
-const message = await openai.beta.threads.messages.create(
-    thread.id,
-    {
-      role: "user",
-      content: "I need to solve the equation `3x + 11 = 14`. Can you help me?"
-    }
-    );
-
-//================================================================================//
-// ========================         Create a run          ========================//
-//================================================================================//
-
-    let run = await openai.beta.threads.runs.createAndPoll(
-        thread.id,
-        { 
-          sebasAssistant_id: sebasAssistant.id,
-          instructions: "Please address the user as Jane Doe. The user has a premium account."
-        }
-        );
-
-   
-//=========================================================================================//
-// Once the Run completes, you can list the Messages added to the Thread by the Assistant  //
-//=========================================================================================//
-
-    if (run.status === 'completed') {
-        const messages = await openai.beta.threads.messages.list(
-          run.thread_id
-        );
-        for (const message of messages.data.reverse()) {
-          console.log(`${message.role} > ${message.content[0].text.value}`);
-        }
-        } else {
-        console.log(run.status);
-        }
+main().catch((error) => console.error("Error en la ejecución de main:", error));
