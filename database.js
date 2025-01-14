3bbc00aed6f31650045c1fa081402ed159d3d55e
SebaPrado <sebastian.pradomelesi@gmail.com>
2025-01-14 16:36:05 +0100

    7

--- database.js
+++ database.js
@@ -13,35 +13,22 @@ const connectionString = process.env.DATABASE_URL;
 
 const sql = postgres(connectionString, {
   ssl: {
-    rejectUnauthorized: false, // Aceptar certificados autofirmados
+    rejectUnauthorized: false, // Acepta certificados autofirmados
   },
 });
+
 let usuarioEspecifico_base_de_datos;
 const obtenerUsuariosDeBaseDeDatos = async (whatsapp_ID) => {
   try {
-    // Consulta los usuarios de la base de datos
-    // let whatsapp_ID = 1234567;
     let usuarios_base_de_datos = await sql`SELECT * FROM "Users";`;
     usuarioEspecifico_base_de_datos = usuarios_base_de_datos.find(
       (user) => user.whatsapp_id == whatsapp_ID
     );
-    // console.log("1)", usuarioEspecifico_base_de_datos);
 
-    // console.log("Usuarios obtenidos:", usuarios_base_de_datos);
     return usuarioEspecifico_base_de_datos;
   } catch (error) {
     console.error("Error al consultar la tabla Users:", error.message);
-  } 
-//   finally {
-//     await sql.end(); // Cierra la conexión
-//   }
+  }
 };
-
-// obtenerUsuariosDeBaseDeDatos(1234567).then((result) =>
-//   console.log("2)", result)
-// );
-// obtenerUsuariosDeBaseDeDatos(3333333).then((result) =>
-//   console.log("3)", result)
-// );
 
 export { obtenerUsuariosDeBaseDeDatos };
