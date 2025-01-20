253fbab8d6895f2b767eb1c611e59c02bda84535
SebaPrado <sebastian.pradomelesi@gmail.com>
2025-01-20 19:58:36 +0100

    2

--- functions/incrementCounter.js
+++ functions/incrementCounter.js
@@ -11,13 +11,6 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
       rejectUnauthorized: false,
     },
   },
-  // Optional but recommended settings
-  pool: {
-    max: 5, // Maximum number of connection in pool
-    min: 0, // Minimum number of connection in pool
-    acquire: 30000, // The maximum time, in milliseconds, that pool will try to get connection before throwing error
-    idle: 10000, // The maximum time, in milliseconds, that a connection can be idle before being released
-  },
 });
 
 const incrementCounter = async (whatsapp_id) => {
